10f4fbe4b765c1c01bc71347b9a3a407bb34760d
Gabriel Gomes da Cunha <ggcunha@localhost.localdomain>
2020-02-21 14:57:41 -0300

    add cors/change mongo connection address

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
-mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-lmjul.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-lmjul.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+mongoose.connect('mongodb://localhost:27017/omnistack10', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
